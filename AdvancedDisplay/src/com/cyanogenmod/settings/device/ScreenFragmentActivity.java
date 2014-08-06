022310580573910ea783b3b837da608539df32c5
Alexander Martinz <eviscerationls@gmail.com>
2014-08-06 19:32:04 +0200

    Revert "Touchwake"
    This reverts commit 6980cf332eb69137de0d25dc95a43c5aac86bb51.
    
    Change-Id: I2b0ba76b9b203de64707583fc3db23daa2eeb6d0

--- AdvancedDisplay/src/com/cyanogenmod/settings/device/ScreenFragmentActivity.java
+++ AdvancedDisplay/src/com/cyanogenmod/settings/device/ScreenFragmentActivity.java
@@ -23,34 +23,22 @@ import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceGroup;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
-import android.preference.SwitchPreference;
 import android.util.Log;
 
 import com.cyanogenmod.settings.device.R;
 
-public class ScreenFragmentActivity
-        extends PreferenceFragment
-        implements OnPreferenceChangeListener {
+public class ScreenFragmentActivity extends PreferenceFragment {
 
     private static final String PREF_ENABLED = "1";
     private static final String TAG = "DisplaySettings_Screen";
-    private static final String KEY_TOUCHWAKE_ENABLE = "touchwake_enable";
-    private static final String KEY_TOUCHWAKE_TIMEOUT = "touchwake_timeout";
-    private static final String FILE_TOUCHWAKE_ENABLE = "/sys/devices/virtual/misc/touchwake/enabled";
-    private static final String FILE_TOUCHWAKE_TIMEOUT = "/sys/devices/virtual/misc/touchwake/delay";
-
     private mDNIeScenario mmDNIeScenario;
     private mDNIeMode mmDNIeMode;
     private mDNIeNegative mmDNIeNegative;
-    private SwitchPreference mTouchwakeEnable;
-    private SeekBarPreference mTouchwakeTimeout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -70,25 +58,6 @@ public class ScreenFragmentActivity
         mmDNIeNegative = (mDNIeNegative) findPreference(DisplaySettings.KEY_MDNIE_NEGATIVE);
         mmDNIeNegative.setEnabled(mDNIeNegative.isSupported(res.getString(R.string.mdnie_negative_sysfs_file)));
 
-        /* Touchwake */
-        mTouchwakeEnable = (SwitchPreference) findPreference(KEY_TOUCHWAKE_ENABLE);
-        mTouchwakeTimeout = (SeekBarPreference) findPreference(KEY_TOUCHWAKE_TIMEOUT);
-
-        if (!isSupported(FILE_TOUCHWAKE_ENABLE)) {
-            mTouchwakeEnable.setEnabled(false);
-            mTouchwakeEnable.setSummary(R.string.kernel_does_not_support);
-
-            mTouchwakeTimeout.setEnabled(false);
-            mTouchwakeTimeout.setSummary(R.string.kernel_does_not_support);
-         } else {
-            int b = Integer.parseInt(Utils.readOneLine(FILE_TOUCHWAKE_ENABLE));
-            mTouchwakeEnable.setChecked(b != 0);
-            mTouchwakeEnable.setOnPreferenceChangeListener(this);
-
-            int i = Integer.parseInt(Utils.readOneLine(FILE_TOUCHWAKE_TIMEOUT));
-            mTouchwakeTimeout.setValue(i / 1000);
-            mTouchwakeTimeout.setOnPreferenceChangeListener(this);
-        }
     }
 
     @Override
@@ -100,33 +69,11 @@ public class ScreenFragmentActivity
         return true;
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mTouchwakeEnable) {
-            boolean b = (Boolean) newValue;
-            mTouchwakeEnable.setChecked(b);
-            Utils.writeValue(FILE_TOUCHWAKE_ENABLE, b);
-        } else if (preference == mTouchwakeTimeout) {
-            int i = (Integer) newValue;
-            mTouchwakeTimeout.setValue(i);
-            String s = Integer.toString (i * 1000);
-            Utils.writeValue(FILE_TOUCHWAKE_TIMEOUT, s);
-        }
-        return true;
-    }
-
     public static boolean isSupported(String FILE) {
         return Utils.fileExists(FILE);
     }
 
     public static void restore(Context context) {
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-        if (isSupported(FILE_TOUCHWAKE_ENABLE)) {
-            boolean b = sharedPrefs.getBoolean(KEY_TOUCHWAKE_ENABLE, false);
-            Utils.writeValue(FILE_TOUCHWAKE_ENABLE, b);
-
-            int i = sharedPrefs.getInt(KEY_TOUCHWAKE_TIMEOUT, 10) * 1000;
-            Utils.writeValue(FILE_TOUCHWAKE_TIMEOUT, Integer.toString(i));
-        }
     }
 }
